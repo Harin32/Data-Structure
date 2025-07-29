9523fed584e60a600ec865474fd5bbdbc92e20a3
Harin32 <150279863+Harin32@users.noreply.github.com>
2025-07-29 14:44:47 +0530

    work7

---
+++ Multiplication.java
@@ -0,0 +1,20 @@
+class Multiplication
+{
+    public static void main(String[] args)
+    {
+        int k=2;
+        int i;
+        while(k<=12)
+        {
+            for( i=1;i<=10;i++)
+            {
+                System.out.println(k +"*"+ i +"=" + (k*i));
+
+
+            }
+            k++;
+        }
+        
+
+    }
+}
