eb68d55b424d45c8a6e2770f0dd527b89bbf8447
Harin32 <150279863+Harin32@users.noreply.github.com>
2025-07-29 14:40:41 +0530

    work4

---
+++ SquareNum.java
@@ -0,0 +1,14 @@
+class work6
+{
+    public static void main(String[] args)
+
+    {
+        int i=1;
+        int n=168;
+        while(i<n)
+        {
+            System.out.println(i);
+            i=i*2;
+        }
+    }
+}
