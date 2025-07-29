e71b79425305a95f6f006e23482c75e641dbf3b8
Harin32 <150279863+Harin32@users.noreply.github.com>
2025-07-29 14:36:52 +0530

    work2

---
+++ dayOfWeek.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class dayOfWeek
+{
+    public static void main(String args[])
+    {
+        
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any number upto 6:");
+        int day = sc.nextInt();
+        
+        if ( day == 1)
+        {
+            System.out.println("Mon");
+        }
+        else if( day == 2)
+        {
+             System.out.println("thu");
+        }
+        else if( day == 3)
+        {
+            System.out.println("wed");
+        }
+        else if( day == 4)
+        {
+            System.out.println("thr");
+        }
+        else if( day==5)
+        {
+            System.out.println("Fri");
+        }
+        else if( day == 6)
+        {
+            System.out.println("sat");
+        }
+        else
+        {
+            System.out.println("sunday");
+        }
+    }
+}
